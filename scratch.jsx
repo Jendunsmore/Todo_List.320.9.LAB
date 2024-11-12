a3f254beb0611b22c75c5066b938f68df03484a3
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-11 22:53:50 -0500

    added scratch file and requirements

---
+++ scratch.jsx
@@ -0,0 +1,26 @@
+/*
+
+Your todo list application must have:
+
+1) A heading labeling it as a "todo list."
+
+2) A list of "todo" items, which are strings listing activities to be accomplished
+    (e.g. "find that missing sock"). Each "todo" item should have:
+
+    2.1) A checkbox next to it which indicates whether it is "complete."
+
+    2.2) A "delete" button next to it which removes it from the list.
+        - The "delete" button should be disabled unless the todo is complete!
+
+    2.3) An "edit" button that replaces the todo string with a text input used to edit the todo.
+        - Hint: bind the value of this text input to a piece of state so that it is always accurate,
+        even when first displayed!
+
+        - When this text input is active, the "delete" and "edit" buttons should be hidden, and a "save"
+        button should appear. The "save" button should save any changes made to the todo within the text input.
+
+3) An input element that creates new todo items and adds them to the list.
+
+4) New todos should be added to the top of the list visually; the oldest todos should be at the bottom.
+
+*/
