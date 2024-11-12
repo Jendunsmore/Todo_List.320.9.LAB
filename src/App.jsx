8ecc9595532e564b8ee76775c44e62c75739216b
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-12 01:12:11 -0500

    finished working up app.jsx, so far...

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ function todoReducer(draft, action) {
                 completed: false,
                 isEditing: false
             });
-// reducer function takes in current draft of state and action object
-// returns new, immutable version of state
+            // reducer function takes in current draft of state and action object
+            // returns new, immutable version of state
 
-// if action.type is add_todo then a new todo item is added to the beginning of the todo list.
-// the new list should have the following:
-// id: current time in milliseconds
-// text: text of todo item. obtained from action.payload
-// completed: false = item isn't completed
-// isEditing: false = todo item isn't currently being edited
+            // if action.type is add_todo then a new todo item is added to the beginning of the todo list.
+            // the new list should have the following:
+            // id: current time in milliseconds
+            // text: text of todo item. obtained from action.payload
+            // completed: false = item isn't completed
+            // isEditing: false = todo item isn't currently being edited
             break;
         case 'TOGGLE_COMPLETE': // toggle completed item status, at specified index
             draft[action.index].completed = !draft[action.index].completed;
@@ -48,22 +48,67 @@ function todoReducer(draft, action) {
 }
 
 function App() {
-    const [newTodo, setNewTodo] = useState(''); // useState hool to make state variable for new item// useState hook is used to create a state variable for the new todo item.
-    const [todos,dispatch] = useImmerReducer)todoReducer, []); // useImmerReducer hook is used to create state and dispatch functions.
-    const [newTodo, setNewTodo] = useState(''); // todos is current state of todo list, an arry of todo items
+    const [todos, dispatch] = useImmerReducer(todoReducer, []);
+    const [newTodo, setNewTodo] = useState('');
 
-    const handleAddTodo = () => { // event handler called when user clicks add todo// handleAddTodo is an event handler that is called when the user clicks the "Add Todo" button.
-        if (newTodo.trim()) { // if new todo exists
     const handleAddTodo = () => {
         if (newTodo.trim()) {
-            dispatch({ type: 'ADD_TODO', payload: newTodo}); // dispatch- a function used ti send an action to the reducer
+            dispatch({ type: 'ADD_TODO', payload: newTodo });
             setNewTodo('');
         }
     };
 
-return (
-    <div className="app-container">
-        <h1 className="heading"> Halloween Todo List</h1>
-        
-    </div>
-)
+    return (
+        <div className="app-container">
+            <h1 className="heading">🎃 Halloween Todo List 🎃</h1>
+
+            <div className="new-todo">
+                <input
+                    type="text"
+                    placeholder="What spooky task to add?"
+                    value={newTodo}
+                    onChange={(e) => setNewTodo(e.target.value)}
+                />
+                <button onClick={handleAddTodo}>Add Todo</button>
+            </div>
+
+            <ul className="todo-list">
+                {todos.map((todo, index) => (
+                    <li key={todo.id} className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+                        <input
+                            type="checkbox"
+                            checked={todo.completed}
+                            onChange={() => dispatch({ type: 'TOGGLE_COMPLETE', index })}
+                        />
+                        {todo.isEditing ? (
+                            <input
+                                type="text"
+                                value={todo.text}
+                                onChange={(e) =>
+                                    dispatch({ type: 'SAVE_TODO', index, payload: e.target.value })
+                                }
+                                onBlur={() => dispatch({ type: 'SAVE_TODO', index, payload: todo.text })}
+                            />
+                        ) : (
+                            <span>{todo.text}</span>
+                        )}
+                        <button
+                            onClick={() => dispatch({ type: 'EDIT_TODO', index })}
+                            disabled={todo.completed}
+                        >
+                            Edit
+                        </button>
+                        <button
+                            onClick={() => dispatch({ type: 'DELETE_TODO', index })}
+                            disabled={!todo.completed}
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
