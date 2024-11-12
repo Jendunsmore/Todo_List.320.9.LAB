e2d035be5a0dd462635f1633cc33752978e79199
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-11 23:56:31 -0500

    editing reducer function for todo items

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,40 @@ import { useImmerReducer } from 'use-immer';
 // create a new, immutable version of the state when the state changes
 
 
+// Reducer function for the todo list items
+function todoReducer(draft, action) {
+    switch (action.type) {
+        case 'ADD_TODO':
+            draft.unshift({
+                id: Date.now(),
+                text: action.payload,
+                completed: false,
+                isEditing: false
+            });
+// reducer function takes in current draft of state and action object
+// returns new, immutable version of state
+
+// if action.type is add_todo then a new todo item is added to the beginning of the todo list.
+// the new list should have the following:
+// id: current time in milliseconds
+// text: text of todo item. obtained from action.payload
+// completed: false = item isn't completed
+// isEditing: false = todo item isn't currently being edited
+            break;
+        case 'TOGGLE_COMPLETE': // toggle completed item status, at specified index
+            draft[action.index].completed = !draft[action.index].completed;
+            break;
+        case 'DELETE_TODO': // delete todo item at specified index
+            draft.splice(action.index, 1);
+            break;
+        case 'EDIT_TODO': // edit todo item at specified index. edit text input
+            draft[action.index].isEditing = true;
+            break;
+        case 'SAVE_TODO': // save todo item at specified index
+            draft[action.index].text = action.payload; // in action.payload
+            draft[action.index].isEditing = false; // isEditing = false, edit text input is hidden
+            break;
+        default:
+            break;
+    }
+}
