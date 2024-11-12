bdd83d670a14e978a31d620fd1496e5a0f299258
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-11-11 22:35:04 -0500

    main.jsx, App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
-import React { useState } from 'react';
-import './App.css';
+// main component- file is main structure of the todo list app. Todo list logic.
+
+import React, { useState } from 'react'; // import useState hook from React - use for state management
+import './App.css'; //import CSS
 import { useImmerReducer } from 'use-immer';
+// import useImmerReducer hook, custom hook for managing state with immer library -
+// allows us to write code that looks like
+// we're writing in-place modifications to the state, but in reality, the hook will always
+// create a new, immutable version of the state when the state changes
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// React's entry point
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './App.css';
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
+
+// createRoot is a function that creates a root container node in the DOM and returns
+// a root object. The root object has a render method that takes a React element and
+// renders it into the container node.
+//
+// The element passed to render is wrapped in a React.StrictMode component. This component
+// tells React to run in strict mode which means that it will check for potential problems
+// in the code and warn about them. Strict mode can be disabled by passing false as the
+// first argument to the createRoot method.
+//
+// The App component is the root component of the application and it is the one that is
+// rendered into the root container node.
